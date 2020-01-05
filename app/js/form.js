f89c019fd15e34277f07f2b258b4ac90ab189f8c
Wolfleader101 <james.morgan@iinet.net.au>
2020-01-05 12:02:45 +0800

    Ways to select options

--- app/js/form.js
+++ app/js/form.js
@@ -7,7 +7,57 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
-lowercase = document.getElementById("lowercase").checked;
-uppercase = document.getElementById("uppercase").checked;
-numbers = document.getElementById("numbers").checked;
-symbols = document.getElementById("symbols").checked;
+function Check() {
+    lowercase = document.getElementById("lowercase").checked;
+    uppercase = document.getElementById("uppercase").checked;
+    numbers = document.getElementById("numbers").checked;
+    symbols = document.getElementById("symbols").checked;
+
+    if (lowercase === true) {
+      console.log("lowercase");
+      console.log("===========================================");
+
+      if (uppercase === true) {
+        console.log("lowercase + uppercase");
+        console.log("===========================================");
+        
+      }
+      if (numbers === true) {
+        console.log("lowercase + numbers");
+        console.log("===========================================");
+      }
+      if (symbols === true) {
+        console.log("lowercase + symbols");
+        console.log("===========================================");
+      }
+
+
+    }else if (uppercase === true) {
+      console.log("uppercase");
+      console.log("===========================================");
+
+      if (numbers === true) {
+        console.log("uppercase + numbers");
+        console.log("===========================================");
+      }
+      if (symbols === true) {
+        console.log("uppercase + symbols");
+        console.log("===========================================");
+      }
+
+      // NUMBERS, SYMBOLS
+    }else if (numbers === true) {
+      console.log("numbers");
+      console.log("===========================================");
+      if (symbols === true) {
+        console.log("numbers + symbols");
+        console.log("===========================================");
+      }
+
+      // SYMBOLS
+    }else if (symbols === true) {
+      console.log("numbers + symbols");
+      console.log("===========================================");
+    }
+
+}
